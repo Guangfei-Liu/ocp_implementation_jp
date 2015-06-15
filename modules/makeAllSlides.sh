502fd8c8b06caffa2f89927add40980bf05f442e
Patrick Rutledge <prutledg@forge.opentlc.com>
2015-06-15 13:48:03 -0400

    fixed script

--- modules/makeAllSlides.sh
+++ modules/makeAllSlides.sh
@@ -4,6 +4,6 @@ for dir in *_*
 do
  cd $dir
  cat /dev/null > AllSlides.txt
- for file in 0*; do echo "include::$file[]" >> AllSlides.txt ; done
+ for file in *.adoc; do echo "include::$file[]" >> AllSlides.txt ; done
  cd ..
 done

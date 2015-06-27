83a47afb5b2ef6ef8dd2ed4c16f5113eef189057
Shachar Borenstein <shachar.borenstein@gmail.com>
2015-06-27 17:39:13 +1000

    Added Clean scripts for Imp class

---
+++ lab_setup/scripts/infranode00-init.sh
@@ -0,0 +1,18 @@
+###############################################################################################################
+#### COMMON Tasks
+# Fix some Internal issues, We don't do anything in implementation
+
+#vars
+###############################################################################################################
+export LOGFILE="/root/.InfraNode00.log"
+###############################################################################################################
+
+echo "Hostname is `hostname`" >> $LOGFILE
+
+# This is the Fix for the SSH issue we have in our environment 
+systemctl restart firewalld
+# add eth0 to public zone
+firewall-cmd --zone public --add-interface=eth0
+
+
+echo "----Completed Host Specific Script `date` "| tee -a $LOGFILE

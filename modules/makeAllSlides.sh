b6f80b1f9a828f6a66657c0189d7c59bfe368100
Patrick Rutledge <prutledg@forge.opentlc.com>
2015-07-10 13:57:19 -0400

    Fixed makeall slides and added sync

--- modules/makeAllSlides.sh
+++ modules/makeAllSlides.sh
@@ -4,6 +4,13 @@ for dir in *_*
 do
  cd $dir
  cat /dev/null > AllSlides.txt
- for file in *.adoc; do echo "include::$file[]" >> AllSlides.txt ; done
+ for file in *.adoc
+ do
+   tst=`echo $file|grep "00_.*Complete"`
+   if [ -z "$tst" ]
+   then
+     echo "include::$file[]" >> AllSlides.txt
+   fi
+ done
  cd ..
 done

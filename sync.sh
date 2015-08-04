eae552248cf428ee91e846e9b7ce0ecd1632fef6
sborenst <shachar.borenstein@gmail.com>
2015-08-04 13:37:32 +1000

    edited output directory in sync.sh

--- sync.sh
+++ sync.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 SDIR="/home/jenkins/jenkins_home/jobs/ose_implementation/lastSuccessful/archive"
-DDIR="/var/www/html/classes/ose3-class"
+DDIR="/home/shacharb-redhat.com/temp/ose3-implementation"
 
 scp -q $SDIR/LabInstructionsFiles/*.html www.opentlc.com:$DDIR/html
-scp -q $SDIR/LabInstructionsFiles/*.pdf www.opentlc.com:$DDIR/pdf
+scp  $SDIR/LabInstructionsFiles/*.pdf www.opentlc.com:$DDIR/pdf
 scp -q $SDIR/scormFiles/*.zip www.opentlc.com:$DDIR/html
 ssh www.opentlc.com 'cd '$DDIR'/html;for zip in *.zip; do dn=`echo $zip|sed 's/.zip//'`;rm -rf '$DDIR'/html/$dn;mkdir -p '$DDIR'/html/$dn;cd '$DDIR'/html/$dn;unzip -oq '$DDIR'/html/$zip;rm -f '$DDIR'/html/$zip;sed -i "s/<audio.*audio>//" '$DDIR'/html/$dn/AllSlides.html; done'
-

b466ea09e4f23673b287402c730e20aabca816c1
sborenst <shachar.borenstein@gmail.com>
2015-08-04 13:23:14 +1000

    Module 07 redone - Labs still needs updates"

--- AsciiDocSplitter.pl
+++ AsciiDocSplitter.pl
@@ -1,24 +1,21 @@
 #!/usr/bin/perl
 ###
-### Usage : ./AsciiDocSplitter.pl SourceFilePath [OutputDirectory] [Labs=1|0]
-### ./AsciiDocSplitter.pl somelargeascii.adoc myprojectname 0
-### ./AsciiDocSplitter.pl somelargeasciiWithLABS.adoc myprojectname 1
+### Usage : ./AsciiDocSplitter.pl SourceFilePath OutputDirectory
+### ./AsciiDocSplitter.pl 03_Installation/00_Module03_Complete.adoc 03_Installation/
+### This script just takes a .adoc file and splits it to seperate files every
+### time it encounters "^== " (regex). It will also populate an AllSlides.txt file.
+### To update the "complete" file with the slides, run:
+###  cat `ls *.adoc | grep -v 00_` > 00_Module0x_Complete.adoc
 
-
-#x="05";
-#git rm `ls ${x}*/*.adoc ${x}*/AllSlides.txt | egrep -iv 'labs|complete'` ; rm -f ${x}*~ ;
-#./AsciiDocSplitter.pl ${x}*/00*.adoc ${x}* ; git add ${x}*/*.adoc ; git add ${x}*/AllSlides.txt  ; git commit -m"Added Module${x} - Ready for Review"
-
-
-if (@ARGV <1)
+if (@ARGV <2)
 	{
-	print("Usage : ./AsciiDocSplitter.pl SourceFilePath ImagesSourceDirectory [OutputDirectory] \n");
+	print("Usage : ./AsciiDocSplitter.pl SourceFilePath OutputDirectory \n");
 	die("not enough arguments\n");
 	}
 
 # VARS
 $AUTHORNAME="Shachar Borenstein";
-$DEBUG=1;
+$DEBUG=0;
 chomp ($SOURCEFILE=$ARGV[0]);
 chomp ($DEFAULT_OUTPUT_DIR=$ARGV[1]); system("mkdir -p $DEFAULT_OUTPUT_DIR");
 $current_index_file="${DEFAULT_OUTPUT_DIR}/AllSlides.txt";
@@ -39,15 +36,16 @@ foreach $line (<SOURCEFILE>)
 
 		$line =~ /^== (.*)$/;
 		$pretty_current_page_name = $current_page_name=$1;
-		if ($pretty_current_page_name =~ /nbsp/) { $pretty_current_page_name = $current_page_name= "Title"; }
+		if ($current_page_name =~ /nbsp/) {$pretty_current_page_name = $current_page_name = "Title" ; }
 		$current_page_name =~ tr/ /_/; # we substitute spaces with _
-
 		$DEBUG && print "Found Page $page_counter: $line \n";
 		$current_file_name="${DEFAULT_OUTPUT_DIR}/${page_counter}_${current_page_name}.adoc";
 		$DEBUG && print "File will be named: $current_file_name\n";
 		open(CURRENTFILE,">>",$current_file_name) or die("could not open: $current_file_name\n");
-		#print CURRENTFILE "
-#";
+		print CURRENTFILE "
+:scrollbar:
+:data-uri:
+";
 
 	if (-e $current_index_file)
 		{

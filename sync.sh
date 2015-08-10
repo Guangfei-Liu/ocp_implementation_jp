e372f3cf5852e053c2c2748aa8b913bab20d7c82
sborenst <shachar.borenstein@gmail.com>
2015-08-10 16:47:17 +1000

    cleanup"

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SDIR="/home/jenkins/jenkins_home/jobs/ose_implementation/lastSuccessful/archive"
-DDIR="/home/shacharb-redhat.com/temp/ose3-implementation"
+DDIR="/var/www/html/classes/ose3-implementation"
 
-scp -q $SDIR/LabInstructionsFiles/*.html www.opentlc.com:$DDIR/html
+scp  $SDIR/LabInstructionsFiles/*.html www.opentlc.com:$DDIR/html
 scp  $SDIR/LabInstructionsFiles/*.pdf www.opentlc.com:$DDIR/pdf
-scp -q $SDIR/scormFiles/*.zip www.opentlc.com:$DDIR/html
+scp $SDIR/scormFiles/*.zip www.opentlc.com:$DDIR/html
 ssh www.opentlc.com 'cd '$DDIR'/html;for zip in *.zip; do dn=`echo $zip|sed 's/.zip//'`;rm -rf '$DDIR'/html/$dn;mkdir -p '$DDIR'/html/$dn;cd '$DDIR'/html/$dn;unzip -oq '$DDIR'/html/$zip;rm -f '$DDIR'/html/$zip;sed -i "s/<audio.*audio>//" '$DDIR'/html/$dn/AllSlides.html; done'

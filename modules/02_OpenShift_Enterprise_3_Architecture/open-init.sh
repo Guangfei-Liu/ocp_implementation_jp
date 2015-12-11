7b0ebedcad212eb373db2dbef1a9f89e24ce3f48
sborenst <shachar.borenstein@gmail.com>
2015-12-11 17:35:01 +1100

    08_Deployment_Lifecycle cosmetics

--- modules/02_OpenShift_Enterprise_3_Architecture/open-init.sh
+++
@@ -1,5 +0,0 @@
-export course=$2;
-export whoiam=`hostname -s| awk -F"-" '{print $1}'`;
-echo curl --connect-timeout 120 -s http://www.opentlc.com/download/${course}/${whoiam}-init.sh
-curl --retry 3 --connect-timeout 120 -s http://www.opentlc.com/download/${course}/${whoiam}-init.sh > /usr/local/bin/${whoiam}-init.sh;
-bash /usr/local/bin/${whoiam}-init.sh $1 $2 $3 $4 
